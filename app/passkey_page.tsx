8f589974c27dd21f96480bb88d2cd4f17980fcd3
noahg1238 <noah.greaves@edu.sait.ca>
2026-07-26 17:15:13 -0600

    Intial app created with required pages as well as basic TODOs for each page.

---
+++ app/passkey_page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { View } from "react-native";
+
+const passkey_page = () => {
+  return <View></View>;
+};
+
+export default passkey_page;
+/* should have the ability to add or change passkey links from settings page */
---
+++ app/year_overview.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { View } from "react-native";
+
+const year_overview = () => {
+  return <View></View>;
+};
+
+export default year_overview;
+
+/*
+year overview page. should display the year the user was on in the 
+month view. The user should be able to go 25 years in either direction.
+*/
