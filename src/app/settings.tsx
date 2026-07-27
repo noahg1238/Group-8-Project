af125f96b1091e23aebe8f8b9c9858534e09e20d
Daniil Sakov <199678175+sakovdaniil628862712342434324@users.noreply.github.com>
2026-07-27 16:53:33 -0600

    For update

--- src/app/settings.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { View } from "react-native";
-
-const settings = () => {
-  return <View></View>;
-};
-export default settings;
-/*settings page should have settings included on prototype plus possibly more.*/
